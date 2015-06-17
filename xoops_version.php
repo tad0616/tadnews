b6734e9a4e690580492aff9b7a5fa97eb83434ef
Tad <tad0616@gmail.com>
2015-06-17 09:21:26 +0800

    修改說明文件

--- xoops_version.php
+++ xoops_version.php
@@ -15,7 +15,7 @@ $modversion['image']       = "images/logo_{$xoopsConfig['language']}.png";
 $modversion['dirname']     = basename(dirname(__FILE__));
 
 //---模組狀態資訊---//
-$modversion['release_date']        = '2015/06/10';
+$modversion['release_date']        = '2015/06/16';
 $modversion['module_website_url']  = 'http://tad0616.net/';
 $modversion['module_website_name'] = _MI_TADNEWS_WEB;
 $modversion['module_status']       = 'release';
