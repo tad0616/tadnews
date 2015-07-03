3891c0d7e59684fedb6af6d28109676b98998c3c
Tad <tad0616@gmail.com>
2015-07-03 14:51:58 +0800

    修正自訂頁面後台無法拉動排序的問題

--- admin/save_sort.php
+++ admin/save_sort.php
@@ -1,13 +1,13 @@
 <?php
-// include_once "header.php";
+include_once "header.php";
 
-// $updateRecordsArray = $_POST['tr'];
+$updateRecordsArray = $_POST['tr'];
 
-// $sort = 1;
-// foreach ($updateRecordsArray as $recordIDValue) {
-//     $sql = "update " . $xoopsDB->prefix("tad_news") . " set `page_sort`='{$sort}' where `nsn`='{$recordIDValue}'";
-//     $xoopsDB->queryF($sql) or die("Save Sort Fail! (" . date("Y-m-d H:i:s") . ")");
-//     $sort++;
-// }
+$sort = 1;
+foreach ($updateRecordsArray as $recordIDValue) {
+    $sql = "update " . $xoopsDB->prefix("tad_news") . " set `page_sort`='{$sort}' where `nsn`='{$recordIDValue}'";
+    $xoopsDB->queryF($sql) or die("Save Sort Fail! (" . date("Y-m-d H:i:s") . ")");
+    $sort++;
+}
 
-// echo "Save Sort OK! (" . date("Y-m-d H:i:s") . ")";
+echo "Save Sort OK! (" . date("Y-m-d H:i:s") . ")";
