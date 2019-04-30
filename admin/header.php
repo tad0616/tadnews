38628f16d8dfd3f9b6b5ecffe277c65ddb319023
Tad <tad0616@gmail.com>
2019-04-30 12:54:30 +0800

    加語系

--- admin/header.php
+++ admin/header.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Tad News module
- *
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
  * which is considered copyrighted (c) material of the original comment or credit authors.
@@ -11,9 +9,9 @@
  *
  * @copyright    XOOPS Project (https://xoops.org)
  * @license      http://www.fsf.org/copyleft/gpl.html GNU public license
- * @package      Tad News
+ * @package
  * @since        2.5.0
- * @author       Tad
+ * @author
  * @version      $Id $
  **/
 require_once dirname(dirname(dirname(__DIR__))) . '/include/cp_header.php';
